044447246f3862af656d2d7ae0fb92b4b0195c74
ktg-UG <ktg27wpolo@gmail.com>
2024-11-01 16:38:54 +0900

    clralify

---
+++ tokenize/learning_text_tokenize.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from janome.tokenizer import Tokenizer
+
+# トークン化用のTokenizerの初期化
+tokenizer = Tokenizer()
+
+# トークン化関数（名詞を抽出)
+def tokenize(text):
+    tokens = tokenizer.tokenize(text)
+    return [token.base_form for token in tokens if token.part_of_speech.startswith('名詞')]
+
+# 学習用データの読み込み
+learning_data_path = r"C:\Users\ktg27\intern\task3\bootcamp課題３＿各種データ - 学習用データ.csv"
+learning_data = pd.read_csv(learning_data_path)
+
+# 学習データの概要文をトークン化して新しい列として追加
+learning_data['tokenized_text'] = learning_data['概要文'].apply(tokenize)
+
+# トークン化されたデータをCSVファイルに保存
+tokenized_data_path = r"C:\Users\ktg27\intern\task3\tokenized_learning_data.csv"
+learning_data[['tokenized_text', '業界']].to_csv(tokenized_data_path, index=False)
